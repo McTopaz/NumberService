09e52cc7b7225cfaa7adc9e0ba9d5269bb248d80
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2023-10-18 00:59:15 +0200

    Reworked all tests. Now testing the services instead of the controllers. This also made it possible to have a unique ApplicationContext to all tests.

--- UnitTest/Tests.cs
+++ UnitTest/Tests.cs
@@ -8,137 +8,121 @@ using System.Web.Http.Results;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebApi;
 using WebApi.Controllers;
+using WebApi.Services;
 
 namespace UnitTest
 {
     [TestClass]
     public class Tests
     {
-        private void ResetPostedNumbers()
-        {
-            //ApplicationContext.UsedNumbers.Clear();
-        }
 
         [TestMethod]
         public void TwoDifferentNumbers_OneAverage()
         {
-            //ResetPostedNumbers();
+            var dict1 = new Dictionary<string, int>()
+            {
+                { "number", 2 }
+            };
 
-            //var dict1 = new Dictionary<string, int>()
-            //{
-            //    { "number", 2 }
-            //};
+            var dict2 = new Dictionary<string, int>()
+            {
+                { "number", 3 }
+            };
 
-            //var dict2 = new Dictionary<string, int>()
-            //{
-            //    { "number", 3 }
-            //};
+            var ac = new ApplicationContext();
+            var pns = new PostNumberService(ac);
+            var gas = new GetAverageService(ac);
 
-            //var pnc = new PostNumberController();
-            //var gac = new GetAverageController();
+            pns.InsertNumber(dict1);
+            pns.InsertNumber(dict2);
+            var result = gas.GetAverage();
 
-            //var resultPnc1 = pnc.Post(dict1);
-            //var resultPnc2 = pnc.Post(dict2);
-            //var result = gac.Get();
-
-            //Assert.IsInstanceOfType(resultPnc1, typeof(OkResult));
-            //Assert.IsInstanceOfType(resultPnc2, typeof(OkResult));
-            //Assert.AreEqual("2,5000", result.First().Value);
+            Assert.AreEqual("2,5000", result.First().Value);
         }
 
         [TestMethod]
         public void TwoEqualNumbers_OneAverage()
         {
-            //ResetPostedNumbers();
+            var dict1 = new Dictionary<string, int>()
+            {
+                { "number", 2 }
+            };
 
-            //var dict1 = new Dictionary<string, int>()
-            //{
-            //    { "number", 2 }
-            //};
+            var dict2 = new Dictionary<string, int>()
+            {
+                { "number", 2 }
+            };
 
-            //var dict2 = new Dictionary<string, int>()
-            //{
-            //    { "number", 2 }
-            //};
+            var ac = new ApplicationContext();
+            var pns = new PostNumberService(ac);
+            var gas = new GetAverageService(ac);
 
-            //var pnc = new PostNumberController();
-            //var gac = new GetAverageController();
+            pns.InsertNumber(dict1);
+            Assert.ThrowsException<Exception>(() => pns.InsertNumber(dict2));
+            var result = gas.GetAverage();
 
-            //var resultPnc1 = pnc.Post(dict1);
-            //var resultPnc2 = pnc.Post(dict2);
-            //var result = gac.Get();
-
-            //Assert.IsInstanceOfType(resultPnc1, typeof(OkResult));
-            //Assert.IsInstanceOfType(resultPnc2, typeof(BadRequestErrorMessageResult));
-            //Assert.AreEqual("2,0000", result.First().Value);
+            Assert.AreEqual("2,0000", result.First().Value);
         }
 
         [TestMethod]
         public void DifferentNumbers_OneAverageWithAllFourDecimals()
         {
             // (1 + 41 + 29) / 3 = 23,66666...666667.
-            // Rounnded to 23,6667.
+            // Rounded to 23,6667.
 
-            //ResetPostedNumbers();
+            var dict1 = new Dictionary<string, int>()
+            {
+                { "number", 1 }
+            };
 
-            //var dict1 = new Dictionary<string, int>()
-            //{
-            //    { "number", 1 }
-            //};
+            var dict2 = new Dictionary<string, int>()
+            {
+                { "number", 41 }
+            };
 
-            //var dict2 = new Dictionary<string, int>()
-            //{
-            //    { "number", 41 }
-            //};
+            var dict3 = new Dictionary<string, int>()
+            {
+                { "number", 29 }
+            };
 
-            //var dict3 = new Dictionary<string, int>()
-            //{
-            //    { "number", 29 }
-            //};
+            var ac = new ApplicationContext();
+            var pnc = new PostNumberService(ac);
+            var gac = new GetAverageService(ac);
 
-            //var pnc = new PostNumberController();
-            //var gac = new GetAverageController();
+            pnc.InsertNumber(dict1);
+            pnc.InsertNumber(dict2);
+            pnc.InsertNumber(dict3);
+            var result = gac.GetAverage();
 
-            //var resultPnc1 = pnc.Post(dict1);
-            //var resultPnc2 = pnc.Post(dict2);
-            //var resultPnc3 = pnc.Post(dict3);
-            //var result = gac.Get();
-
-            //Assert.IsInstanceOfType(resultPnc1, typeof(OkResult));
-            //Assert.IsInstanceOfType(resultPnc2, typeof(OkResult));
-            //Assert.IsInstanceOfType(resultPnc3, typeof(OkResult));
-            //Assert.AreEqual("23,6667", result.First().Value);
+            Assert.AreEqual("23,6667", result.First().Value);
         }
 
         [TestMethod]
         public void OneNumber_OneAverage()
         {
-            //ResetPostedNumbers();
+            var dict1 = new Dictionary<string, int>()
+            {
+                { "number", 2 }
+            };
 
-            //var dict1 = new Dictionary<string, int>()
-            //{
-            //    { "number", 2 }
-            //};
+            var ac = new ApplicationContext();
+            var pnc = new PostNumberService(ac);
+            var gac = new GetAverageService(ac);
 
-            //var pnc = new PostNumberController();
-            //var gac = new GetAverageController();
+            pnc.InsertNumber(dict1);
+            var result = gac.GetAverage();
 
-            //var resultPnc = pnc.Post(dict1);
-            //var result = gac.Get();
-
-            //Assert.IsInstanceOfType(resultPnc, typeof(OkResult));
-            //Assert.AreEqual("2,0000", result.First().Value);
+            Assert.AreEqual("2,0000", result.First().Value);
         }
 
         [TestMethod]
         public void NoNumbers_NoAverage()
         {
-            //ResetPostedNumbers();
+            var ac = new ApplicationContext();
+            var gac = new GetAverageService(ac);
+            var result = gac.GetAverage();
 
-            //var gac = new GetAverageController();
-            //var result = gac.Get();
-
-            //Assert.AreEqual("0,0000", result.First().Value);
+            Assert.AreEqual("0,0000", result.First().Value);
         }
     }
 }
